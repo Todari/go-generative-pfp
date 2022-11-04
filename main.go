6f87dc93c734741b4b612196dcbb9823528ee5fc
이태훈 <rhymint@gmail.com>
2022-11-04 14:49:49 +0900

    rarity별 trait 그룹 추가

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"image"
 	"image/draw"
@@ -17,6 +18,15 @@ import (
 
 	"github.com/Todari/go-generative-pfp/module"
 )
+
+func reset() {
+	if !os.IsExist(os.RemoveAll("./result/")) {
+		os.Mkdir("./result/", 0777)
+	}
+	if !os.IsExist(os.RemoveAll("./json/")) {
+		os.Mkdir("./json/", 0777)
+	}
+}
 
 func openAndDecode(imgPath string) image.Image {
 	img, err := os.Open(imgPath)
@@ -43,36 +53,60 @@ type Trait struct {
 
 func main() {
 
-	var traits = [7]string{"1. background", "2. item", "3. body", "4. clothes", "5. hair", "6. eye", "7. hat"}
-	totalNum := 2000
+	doReset := true
+	totalNum := 1000
+
+	if doReset {
+		reset()
+	}
+	var rarities = [6]string{"1. legend", "2. prime", "3. master", "4. expert", "5. junior", "6. rookie"}
+	var traits = [11]string{"0. legend", "1. background", "2. body", "3. outfit", "4. backpack", "5. pet", "6. hat", "7. eye", "8. mouth", "9. ring", "10. rarity"}
 	var dnaArr []string
 
 	//trait이름 별 weight를 map으로 만들어 줌
-	weightForTrait := make(map[string]int)
+	// weightForTrait := make(map[string]int)
 
-	traitsArr := make([][]string, len(traits))
+	var traitsArr [6][11][]string
+	rarityCounter := make([]int, 6)
 	images := make([]string, len(traits))
 	dnaExist := false
+	rarityFull := false
 	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
-	for i, trait := range traits {
-		files, _ := ioutil.ReadDir("./imgs/" + trait)
-		for _, file := range files {
-			traitsArr[i] = append(traitsArr[i], file.Name())
-			traitName := strings.Split(file.Name(), "#")[0]
-			traitWeight, _ := strconv.Atoi(strings.Split(strings.Split(file.Name(), "#")[1], ".")[0])
-			weightForTrait[traitName] = traitWeight
+
+	for i, rarity := range rarities {
+		for j, trait := range traits {
+			files, _ := ioutil.ReadDir("./imgs/" + rarity + "/" + trait)
+			for _, file := range files {
+				traitsArr[i][j] = append(traitsArr[i][j], file.Name())
+				// traitName := strings.Split(file.Name(), "#")[0]
+				// traitWeight, _ := strconv.Atoi(strings.Split(strings.Split(file.Name(), "#")[1], ".")[0])
+				// weightForTrait[traitName] = traitWeight
+			}
 		}
 	}
+	fmt.Print(traitsArr)
+
+	file, err := os.Create("./metadata.csv")
+	if err != nil {
+		panic(err)
+	}
+	csvw := csv.NewWriter(bufio.NewWriter((file)))
+	var csvCell [][]string
+	defer csvw.Flush()
 
 	for i := 0; i < totalNum; i++ {
 		var dna string
+		var raritySelecter int
 		dnaExist = false
+		rarityFull = false
+		rand.Seed(time.Now().UnixNano())
+		raritySelecter = rand.Intn(6)
 
-		for i, _ := range traitsArr {
+		for i, _ := range traitsArr[raritySelecter] {
 			rand.Seed(time.Now().UnixNano())
-			selecter := rand.Intn(len(traitsArr[i]))
-			images[i] = traitsArr[i][selecter]
+			selecter := rand.Intn(len(traitsArr[raritySelecter][i]))
+			images[i] = traitsArr[raritySelecter][i][selecter]
 			var dna2 string
 			if selecter < 16 {
 				dna2 = "0" + strconv.FormatInt(int64(selecter), 16)
@@ -81,18 +115,71 @@ func main() {
 			}
 			dna += dna2
 		}
+		dna = dna[0:len(dna)-2] + strconv.Itoa(raritySelecter)
 		for _, v := range dnaArr {
 			if dna == v {
 				dnaExist = true
 				break
 			}
 		}
+		switch raritySelecter {
+		case 0:
+			if rarityCounter[raritySelecter] == 1 {
+				rarityFull = true
+			}
+		case 1:
+			if rarityCounter[raritySelecter] == 10 {
+				rarityFull = true
+			}
+		case 2:
+			if rarityCounter[raritySelecter] == 70 {
+				rarityFull = true
+			}
+		case 3:
+			if rarityCounter[raritySelecter] == 120 {
+				rarityFull = true
+			}
+		case 4:
+			if rarityCounter[raritySelecter] == 300 {
+				rarityFull = true
+			}
+		case 5:
+			if rarityCounter[raritySelecter] == 499 {
+				rarityFull = true
+			}
+		}
 
-		if dnaExist == true {
+		if rarityFull {
+			fmt.Println("RARITY FULL")
+			i--
+			continue
+		} else {
+			rarityCounter[raritySelecter]++
+		}
+
+		if dnaExist {
 			fmt.Println("DNA EXIST")
 			i--
 			continue
 		}
+		fmt.Print(rarityCounter)
+
+		csvItem := []string{
+			strconv.Itoa(i),
+			strings.Split(images[0], "#")[0],
+			strings.Split(images[1], "#")[0],
+			strings.Split(images[2], "#")[0],
+			strings.Split(images[3], "#")[0],
+			strings.Split(images[4], "#")[0],
+			strings.Split(images[5], "#")[0],
+			strings.Split(images[6], "#")[0],
+			strings.Split(images[7], "#")[0],
+			strings.Split(images[8], "#")[0],
+			strings.Split(images[9], "#")[0],
+			strings.Split(images[10], "#")[0],
+		}
+		csvCell = append(csvCell, csvItem)
+		// fmt.Print(csvCell)
 
 		module.Json_generator(images, i)
 
@@ -103,7 +190,7 @@ func main() {
 		decodedImages := make([]image.Image, len(images))
 
 		for i, v := range images {
-			decodedImages[i] = openAndDecode("./imgs/" + traits[i] + "/" + v)
+			decodedImages[i] = openAndDecode("./imgs/" + rarities[raritySelecter] + "/" + traits[i] + "/" + v)
 		}
 
 		bounds := decodedImages[0].Bounds()
@@ -125,7 +212,5 @@ func main() {
 		sort.Strings(dnaArr)
 
 	}
-
-	fmt.Println(dnaArr)
-	fmt.Println(len(dnaArr))
+	defer csvw.WriteAll(csvCell)
 }
--- module/json-generator.go
+++ module/json-generator.go
@@ -33,26 +33,37 @@ type Files struct {
 }
 
 func Json_generator(traitArr []string, num int) {
+	// csv
+
+	// json
 	var result Metadata
-	result.Attributes = make([]Attributes, 7)
+	result.Attributes = make([]Attributes, 11)
 	result.Properties.Files = make([]Files, 1)
 
 	result.Symbol = "HUHU"
 	result.Description = "HUHU and friends"
-	result.Attributes[0].Trait_type = "background"
+	result.Attributes[0].Trait_type = "legend"
 	result.Attributes[0].Value = strings.Split(traitArr[0], "#")[0]
-	result.Attributes[1].Trait_type = "item"
+	result.Attributes[1].Trait_type = "background"
 	result.Attributes[1].Value = strings.Split(traitArr[1], "#")[0]
 	result.Attributes[2].Trait_type = "body"
 	result.Attributes[2].Value = strings.Split(traitArr[2], "#")[0]
-	result.Attributes[3].Trait_type = "clothes"
+	result.Attributes[3].Trait_type = "outfit"
 	result.Attributes[3].Value = strings.Split(traitArr[3], "#")[0]
-	result.Attributes[4].Trait_type = "hair"
+	result.Attributes[4].Trait_type = "backpack"
 	result.Attributes[4].Value = strings.Split(traitArr[4], "#")[0]
-	result.Attributes[5].Trait_type = "eye"
+	result.Attributes[5].Trait_type = "pet"
 	result.Attributes[5].Value = strings.Split(traitArr[5], "#")[0]
 	result.Attributes[6].Trait_type = "hat"
 	result.Attributes[6].Value = strings.Split(traitArr[6], "#")[0]
+	result.Attributes[7].Trait_type = "eye"
+	result.Attributes[7].Value = strings.Split(traitArr[7], "#")[0]
+	result.Attributes[8].Trait_type = "mouth"
+	result.Attributes[8].Value = strings.Split(traitArr[8], "#")[0]
+	result.Attributes[9].Trait_type = "ring"
+	result.Attributes[9].Value = strings.Split(traitArr[9], "#")[0]
+	result.Attributes[10].Trait_type = "rarity"
+	result.Attributes[10].Value = strings.Split(traitArr[10], "#")[0]
 
 	result.Properties.Files[0].Type = "image/png"
 	result.Name = "HUHU#" + strconv.Itoa(num)
@@ -60,9 +71,10 @@ func Json_generator(traitArr []string, num int) {
 	result.Properties.Files[0].Uri = strconv.Itoa(num) + ".png"
 	doc, _ := json.Marshal(result)
 
-	err := ioutil.WriteFile("./json/"+strconv.Itoa(num)+".json", doc, os.FileMode(0644))
-	if err != nil {
-		fmt.Println(err)
+	err2 := ioutil.WriteFile("./json/"+strconv.Itoa(num)+".json", doc, os.FileMode(0644))
+
+	if err2 != nil {
+		fmt.Println(err2)
 		return
 	}
 }
